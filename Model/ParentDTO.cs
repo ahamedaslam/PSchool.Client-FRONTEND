6545e617aecf5bbc4c72e65476fb0742e093a8bb
Gulam <Alghouse>
2025-10-27 05:51:43 +0100

    Add project files.

---
+++ Model/ParentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Pschool.Client.Model
+{
+    public class ParentDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
